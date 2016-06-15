7257f95ca26870b52a23564b000aa51a917fa486
nanoc812 <nanoc812@users.noreply.github.com>
2016-06-16 00:26:00 +0800

    some paras

---
+++ config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 16 00:17:07 2016
+
+@author: calm
+"""
+
+# the filepath where all your python files and  the 'images' folder are
+path = '/home/calm/Documents/CARS/train/'
