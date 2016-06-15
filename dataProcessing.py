873ef1fd42b2e5a06ed9aa336280d53d26a76f7a
nanoc812 <nanoc812@users.noreply.github.com>
2016-06-16 00:29:27 +0800

    Prepare the logo dataset

---
+++ dataProcessing.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 12 14:06:46 2016
+
+@author: calm
+"""
+
+from logoSet import imgPreprocess
+from confir import path
+
+###############################################################################
+### prepare the logo dataset
+### All images are in the folder named 'images'
+imgPreprocess(path +'images/')
