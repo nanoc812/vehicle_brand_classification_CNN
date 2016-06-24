7f9e4b863e0b2fb528dec418c1446ea6fa0cf188
nanoc812 <nanoc812@users.noreply.github.com>
2016-06-24 22:52:05 +0800

    add a name of the folder in whicn contains images

--- dataProcessing.py
+++ dataProcessing.py
@@ -11,4 +11,7 @@ from config import path
 ###############################################################################
 ### prepare the logo dataset
 ### All images are in the folder named 'images'
-imgPreprocess(path +'images/')
+imgPreprocess(path +'images/', logo)
+
+# imgPreprocess(path +'images/', logo_test)
+# to generate test images
